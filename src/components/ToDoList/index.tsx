eb660ae68f0762d61afc97b06687e5ff4f788171
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-07-11 14:20:43 -0300

    feat: separar filtros da lista em dispositivos móveis

--- src/components/ToDoList/index.tsx
+++ src/components/ToDoList/index.tsx
@@ -11,29 +11,37 @@ const todos = [
 function ToDoList() {
     const { theme } = useContext(ThemeContext);
     return (
-        <div className={`${themeConfig[theme].todo.backgroundColor} rounded-md`}>
-            <ul>
-                {
-                    todos.map((todo) => (
-                        <li key={todo.id} className={`p-6 border-b ${themeConfig[theme].todo.borderColor}`}>
-                            <div className="flex items-center gap-4">
-                                <button className={`w-6 h-6 border ${themeConfig[theme].todo.borderColor} rounded-full cursor-pointer`}></button>
-                                <p className={themeConfig[theme].todo.textColor}>{todo.text}</p>
-                            </div>
-                        </li>
-                    ))
-                }
-            </ul>
-            <div className={`text-sm flex justify-between items-center p-4 ${themeConfig[theme].layout.textColor}`}>
-                <p>{todos.length} items total</p>
-                <div className="flex gap-4">
-                    <button className="text-bright-blue">All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
+        <>
+            <div className={`${themeConfig[theme].todo.backgroundColor} rounded-md`}>
+                <ul>
+                    {
+                        todos.map((todo) => (
+                            <li key={todo.id} className={`p-6 border-b ${themeConfig[theme].todo.borderColor}`}>
+                                <div className="flex items-center gap-4">
+                                    <button className={`w-6 h-6 border ${themeConfig[theme].todo.borderColor} rounded-full cursor-pointer`}></button>
+                                    <p className={themeConfig[theme].todo.textColor}>{todo.text}</p>
+                                </div>
+                            </li>
+                        ))
+                    }
+                </ul>
+                <div className={`text-sm flex justify-between items-center p-4 ${themeConfig[theme].layout.textColor}`}>
+                    <p>{todos.length} items total</p>
+                    <div className="hidden sm:flex gap-4">
+                        <button className="text-bright-blue">All</button>
+                        <button>Active</button>
+                        <button>Completed</button>
+                    </div>
+                    <button>Clear Selected</button>
                 </div>
-                <button>Clear Selected</button>
             </div>
-        </div>
+
+            <div className={`${themeConfig[theme].todo.backgroundColor} flex justify-center gap-5 py-4 sm:hidden ${themeConfig[theme].layout.textColor} rounded-md mt-4`}>
+                <button className="text-bright-blue">All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </>
     )
 };
 
