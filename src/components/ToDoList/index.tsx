375722fa9344e3cdc3a7453f1aecee6abb3d15b3
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-07-18 13:59:46 -0300

    feat: adicionar hover aos botões de tarefas e filtros

--- src/components/ToDoList/index.tsx
+++ src/components/ToDoList/index.tsx
@@ -18,7 +18,9 @@ function ToDoList() {
                         todos.map((todo) => (
                             <li key={todo.id} className={`p-6 border-b ${themeConfig[theme].todo.borderColor}`}>
                                 <div className="flex items-center gap-4">
-                                    <button className={`w-6 h-6 border ${themeConfig[theme].todo.borderColor} rounded-full cursor-pointer`}></button>
+                                    <span className="w-6 h-6 rounded-full hover:bg-[linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%))] hover:p-px">
+                                        <button className={`w-full h-full border ${themeConfig[theme].todo.borderColor} rounded-full cursor-pointer ${themeConfig[theme].todo.backgroundColor}`}></button>
+                                    </span>
                                     <p className={themeConfig[theme].todo.textColor}>{todo.text}</p>
                                 </div>
                             </li>
@@ -28,18 +30,18 @@ function ToDoList() {
                 <div className={`text-sm flex justify-between items-center p-4 ${themeConfig[theme].layout.textColor}`}>
                     <p>{todos.length} items total</p>
                     <div className="hidden sm:flex gap-4">
-                        <button className="text-bright-blue">All</button>
-                        <button>Active</button>
-                        <button>Completed</button>
+                        <button className={`text-bright-blue cursor-pointer ${theme === "dark" ? "hover:text-dark-purple-100" : "hover:text-light-navy-850"}`}>All</button>
+                        <button className={`cursor-pointer ${theme === "dark" ? "hover:text-dark-purple-100" : "hover:text-light-navy-850"}`}>Active</button>
+                        <button className={`cursor-pointer ${theme === "dark" ? "hover:text-dark-purple-100" : "hover:text-light-navy-850"}`}>Completed</button>
                     </div>
-                    <button>Clear Selected</button>
+                    <button className={`cursor-pointer ${theme === "dark" ? "hover:text-dark-purple-100" : "hover:text-light-navy-850"}`}>Clear Selected</button>
                 </div>
             </div>
 
             <div className={`${themeConfig[theme].todo.backgroundColor} flex justify-center gap-5 py-4 sm:hidden ${themeConfig[theme].layout.textColor} rounded-md mt-4`}>
-                <button className="text-bright-blue">All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button className={`text-bright-blue cursor-pointer ${theme === "dark" ? "hover:text-dark-purple-100" : "hover:text-light-navy-850"}`}>All</button>
+                <button className={`cursor-pointer ${theme === "dark" ? "hover:text-dark-purple-100" : "hover:text-light-navy-850"}`}>Active</button>
+                <button className={`cursor-pointer ${theme === "dark" ? "hover:text-dark-purple-100" : "hover:text-light-navy-850"}`}>Completed</button>
             </div>
         </>
     )
