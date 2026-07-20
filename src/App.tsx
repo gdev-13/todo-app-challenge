1d7d98bbedb6de4f64df303dd1bcc267f3c35f61
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-07-20 11:06:49 -0300

    feat: criar estado e lógica para adicionar tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import { useState, type FormEvent } from "react";
 import ToDoContainer from "./components/ToDoContainer";
 import ToDoHeader from "./components/ToDoHeader";
 import ToDoInput from "./components/ToDoInput";
 import ToDoList from "./components/ToDoList";
 
+interface ToDo {
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
 function App() {
+
+    const [todos, setTodos] = useState<ToDo[]>([]);
+    const addTodo = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const todoText = formData.get("todo") as string;
+        if (!todoText.trim()) return;
+        console.log(todoText);
+        setTodos(prevTodos => [...prevTodos, { id: Date.now(), text: todoText, completed: false }]);
+    }
+
     return (
         <ToDoContainer>
             <ToDoHeader />
-            <ToDoInput />
+            <ToDoInput addTodo={addTodo} />
             <ToDoList />
         </ToDoContainer>
     )
